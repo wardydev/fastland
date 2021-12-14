2160dd447231ce2f8378fe861356d263bd99e124
wardy dev <wardydev@gmail.com>
2021-12-14 22:43:07 +0800

    graph ql testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,26 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react'
+import { graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+export default function Home({data}) {
+  console.log(data)
+  const {author, title} = data.site.siteMetadata
+  return (
+    <div className='bg-red-500'>
+      <h1>{author}</h1>
+      <p>{title}</p>
+    </div>
+  )
+}
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        author
+        description
+        siteUrl
+        title
+      }
+    }
+  }
+`
