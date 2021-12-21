fed1a8e8be010ce611b521b50267419278ebc967
wardy dev <wardydev@gmail.com>
2021-12-21 08:15:56 +0800

    gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Fastlands`,
+        short_name: `Fastlands`,
         start_url: `/`,
         background_color: `#663399`,
         display: `minimal-ui`,
